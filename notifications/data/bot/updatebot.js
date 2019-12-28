f45b53d188d7dfbcf0c3e35a25882b3f2ecb4c2e
Mydoge <LoganLMB2008@hotmail.com>
2019-12-27 21:33:46 -0500

    Update updatebot.js

--- notifications/data/bot/updatebot.js
+++ notifications/data/bot/updatebot.js
@@ -2,7 +2,7 @@ document.querySelector('#show_button').addEventListener('click', function() {
   if (window.webkitNotifications.checkPermission() == 0) { // 0 is PERMISSION_ALLOWED
     // function defined in step 2
     window.webkitNotifications.createNotification(
-        'discord.png', 'Notification Title', 'Notification content...');
+        'discord.png', 'Info', 'OK Bot is changing. The original OK Bot wont be used until ever needed. Please add the new one to youre server to continue using OK Bot.');
   } else {
     window.webkitNotifications.requestPermission();
   }
